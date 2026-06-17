f82cf8860319ab5b4b1e759506b295134e31894b
IsaacPaul S <paulisaac936@gmail.com>
2026-06-17 20:37:58 +0530

    Added Module 2 Data Structures and Algorithms exercises

---
+++ Inventory Management System/Product.java
@@ -0,0 +1,20 @@
+class Product {
+    int productId;
+    String productName;
+    int quantity;
+    double price;
+
+    Product(int productId, String productName, int quantity, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String toString() {
+        return "ID: " + productId +
+               ", Name: " + productName +
+               ", Quantity: " + quantity +
+               ", Price: " + price;
+    }
+}
