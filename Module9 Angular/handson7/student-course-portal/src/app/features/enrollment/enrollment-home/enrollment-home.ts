f95beaaf26f8b3a7da68e94ac53dfae728818939
IsaacPaul S <paulisaac936@gmail.com>
2026-07-24 19:44:32 +0530

    Delete Module9 Angular directory

--- Module9 Angular/handson7/student-course-portal/src/app/features/enrollment/enrollment-home/enrollment-home.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-enrollment-home',
-  imports: [],
-  templateUrl: './enrollment-home.html',
-  styleUrl: './enrollment-home.css',
-})
-export class EnrollmentHome {}
